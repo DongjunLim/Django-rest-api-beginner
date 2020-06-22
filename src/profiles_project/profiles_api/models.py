5767a5ca8b3b4676aca60fcf06e149aff851ed54
DongjunLim <lacuna7304@gmail.com>
2020-06-22 15:52:45 +0900

    create first APIView

--- src/profiles_project/profiles_api/models.py
+++ src/profiles_project/profiles_api/models.py
@@ -18,11 +18,11 @@ class UserProfileManager(BaseUserManager):
         user = self.model(email=email, name=name)
 
         user.set_password(password)
-        user.save(using=self._db)
+        user.save()
 
         return user
 
-    def create_super_user(self, email, name, password=None):
+    def create_superuser(self, email, name, password=None):
         """Create and save a new superuser with given details. """
 
         user = self.create_user(email, name, password)
@@ -30,7 +30,7 @@ class UserProfileManager(BaseUserManager):
         user.is_superuser = True
         user.is_staff = True
 
-        user.save(Using=self._db)
+        user.save()
 
 
 class UserProfile(AbstractBaseUser, PermissionsMixin):
